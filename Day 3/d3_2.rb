67a690de09a1eec849360ed5b7cb3133c473ce4f
jamart <jamartbr@gmail.com>
2022-12-03 21:36:44 +0100

    updating

--- Day 3/d3_2.rb
+++ Day 3/d3_2.rb
@@ -1,13 +1,1 @@
 p File.open('input3.txt').readlines.map(&:chars).each_slice(3).map{(_1.reduce(:&)[0].ord-38)%58}.sum
-
-=begin
-a-z=1-26
-A-Z=27-52
-
-'a'=97  'z'=122 'A'=65  'Z'=90
-
-'a'-'A'=32  'z'-'a'=25
-
-x=(x.ord-65+27
-        -38
-=end
